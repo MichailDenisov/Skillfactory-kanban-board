775e81f2998de037e6f5b2f2ff946cbb8a5f76a2
MichailDenisov <104084982+MichailDenisov@users.noreply.github.com>
2023-07-02 13:41:09 +0500

    first commit

---
+++ my-app/src/components/main/Main.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route} from 'react-router-dom'
+import Board from '../board/Board'
+import TaskDetail from '../task-detail/TaskDetail'
+import css from './Main.module.css'
+
+
+const Main = (props) => {
+	return (
+		<main className={css.main}>
+			<Routes>
+				{/* <Route exact path={'/'}>
+					<Board {...props} />
+				</Route> */}
+				 <Route path="/" element={<Board {...props} />} /> 
+				{/* <Route path={'/tasks/:taskId'}>
+					<TaskDetail {...props} />
+				</Route> */}
+				<Route path="/" element={<TaskDetail {...props} />} />
+			</Routes>
+		</main>
+		
+	)
+}
+
+export default Main
